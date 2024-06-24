cbd3ec19709b5c42c375d5fc57e71f3659e02db8
Hector Salas <salsegovi21@gmail.com>
2024-06-23 23:44:47 -0400

    se actualiza el build

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -13,12 +13,12 @@ const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) {
-        return res.status(401).json({ error:true, message: 'No Autorizado. Por favor inicie sesion para acceder' });
+        return res.status(401).json({ error: 'No Autorizado' });
     }
     jsonwebtoken_1.default.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
             console.error('error en la autenticacion: ', err);
-            return res.status(403).json({ error:true, message: 'Token Invalido: No tienes acceso a este recurso' });
+             return res.status(403).json({ error: 'No tienes acceso a este recurso' });
         }
         next();
     });
